e97280354bb342f12b598e311fd405a12b659521
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-02-26 16:19:24 -0300

    Breakingpoints condicionais e debug console

--- dia-1/b_bug_no_cod.py
+++ dia-1/b_bug_no_cod.py
@@ -1,16 +1,16 @@
 import random
 
 DIGITS_MAP = {
-    0: "zero",
-    1: "um",
-    2: "dois",
-    3: "três",
-    4: "quatro",
-    5: "cinco",
-    6: "seis",
-    7: "sete",
-    8: "oito",
-    9: "nove",
+    '0': "zero",
+    '1': "um",
+    '2': "dois",
+    '3': "três",
+    '4': "quatro",
+    '5': "cinco",
+    '6': "seis",
+    '7': "sete",
+    '8': "oito",
+    '9': "nove",
 }
 
 
